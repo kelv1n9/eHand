755a1408343298816c4ffb6220fcccf3e0ea9626
Elvin Gadirov <gadirov190102@gmail.com>
2025-08-27 15:35:26 +0400

    Code reform and improvement

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <RF24.h>
 #include <RF24Audio.h>
-#include "printf.h"
 #include "EncButton.h"
 #include <EEPROM.h>
+
+#define DEBUG_eHand
+
+#ifdef DEBUG_eHand
+#include "printf.h"
+#define DBG(...)         \
+  do                     \
+  {                      \
+    printf(__VA_ARGS__); \
+  } while (0)
+#else
+#define DBG(...) \
+  do             \
+  {              \
+  } while (0)
+#endif
 
 // Pin definitions
 // A0 - Microphone input
@@ -38,12 +53,13 @@ struct Settings
 {
   uint8_t channel;
   uint8_t volume;
-  uint8_t rateIdx;
+  uint8_t dataRateIdx;
+  uint8_t txPowerIdx;
 };
 
 Settings config;
-bool cfgDirty = false;
-uint32_t cfgLastChange = 0;
+bool isConfigEdited = false;
+uint32_t configLastChangeTime = 0;
 
 Button PTT(PTT_BUTTON_PIN);
 EncButton encoder(ENCODER_A_PIN, ENCODER_B_PIN, ENCODER_SWITCH_PIN);
@@ -51,49 +67,20 @@ EncButton encoder(ENCODER_A_PIN, ENCODER_B_PIN, ENCODER_SWITCH_PIN);
 RF24 radio(CE_PIN, CSN_PIN);
 RF24Audio rfAudio(radio, 0);
 
+bool isTx = false;
 uint8_t channel = CHANNEL_START;
 uint8_t volume = 4;
-uint8_t rateIdx = 1;
+uint8_t dataRateIdx = 1;
+uint8_t txPowerIdx = 3;
 
-void markCfgDirty()
-{
-  cfgDirty = true;
-  cfgLastChange = millis();
-}
-
-void loadSettingsOrDefaults()
-{
-  EEPROM.get(0, config);
-  bool ok = (config.volume <= 7) &&
-            (config.rateIdx <= 2) &&
-            (config.channel >= CH_MIN && config.channel <= CH_MAX);
-
-  if (!ok)
-  {
-    config.volume = volume;
-    config.rateIdx = rateIdx;
-    config.channel = channel;
-    EEPROM.put(0, config);
-  }
-
-  volume = config.volume;
-  rateIdx = config.rateIdx;
-  channel = config.channel;
-}
-
-void maybeSaveSettings()
-{
-  if (cfgDirty && (millis() - cfgLastChange >= SAVE_DELAY_MS))
-  {
-    EEPROM.put(0, config);
-    cfgDirty = false;
-  }
-}
-
-void applyRadio()
+void applyChannel()
 {
   radio.setChannel(channel);
-  switch (rateIdx)
+}
+
+void applyDataRate()
+{
+  switch (dataRateIdx)
   {
   case 0:
     radio.setDataRate(RF24_250KBPS);
@@ -107,89 +94,180 @@ void applyRadio()
   }
 }
 
-void applyVolume(uint8_t vol)
+void applyTxPower()
 {
-  rfAudio.setVolume(vol);
+  switch (txPowerIdx)
+  {
+  case 0:
+    radio.setPALevel(RF24_PA_MIN);
+    break;
+  case 1:
+    radio.setPALevel(RF24_PA_LOW);
+    break;
+  case 2:
+    radio.setPALevel(RF24_PA_HIGH);
+    break;
+  case 3:
+    radio.setPALevel(RF24_PA_MAX);
+    break;
+  }
+}
+
+void applyVolume()
+{
+  rfAudio.setVolume(volume);
+}
+
+void markConfigEdited()
+{
+  isConfigEdited = true;
+  configLastChangeTime = millis();
+}
+
+void loadSettings()
+{
+  EEPROM.get(0, config);
+  bool ok = (config.volume <= 7) &&
+            (config.dataRateIdx <= 2) &&
+            (config.channel >= CH_MIN && config.channel <= CH_MAX) &&
+            (config.txPowerIdx <= 3);
+
+  if (!ok)
+  {
+    config.volume = volume;
+    config.dataRateIdx = dataRateIdx;
+    config.channel = channel;
+    config.txPowerIdx = txPowerIdx;
+    EEPROM.put(0, config);
+  }
+
+  volume = config.volume;
+  dataRateIdx = config.dataRateIdx;
+  channel = config.channel;
+  txPowerIdx = config.txPowerIdx;
+
+  applyTxPower();
+  applyDataRate();
+  applyChannel();
+  applyVolume();
+}
+
+void saveSettings()
+{
+  if (isConfigEdited && (millis() - configLastChangeTime >= SAVE_DELAY_MS))
+  {
+    EEPROM.put(0, config);
+    isConfigEdited = false;
+  }
 }
 
 void setup()
 {
+#ifdef DEBUG_eHand
   Serial.begin(115200);
-
   printf_begin();
+  delay(1000);
+#endif
 
   radio.begin();
   rfAudio.begin();
 
-  loadSettingsOrDefaults();
-  applyRadio();
-  applyVolume(volume);
+  loadSettings();
 
+  DBG("Channel: %u, Volume: %u, dataRateIdx: %u, TxPowerIdx: %u\n", channel, volume, dataRateIdx, txPowerIdx);
+
+#ifdef DEBUG_eHand
   radio.printDetails();
+#endif
+
+  rfAudio.receive();
+  isTx = false;
 }
 
 void loop()
 {
-  if (encoder.tick())
+  encoder.tick();
+  PTT.tick();
+
+  if (PTT.pressing())
   {
-    switch (encoder.action())
+    // Power toggle when encoder turned while PTT held
+    if (encoder.turn())
     {
-    case EB_TURN:
+      int step = (encoder.fast() ? 2 : 1) * encoder.dir();
+      int idx = (int)txPowerIdx + (step > 0 ? 1 : -1);
+      if (idx < 0)
+        idx = 3;
+      if (idx > 3)
+        idx = 0;
+      txPowerIdx = (uint8_t)idx;
+      config.txPowerIdx = txPowerIdx;
+      applyTxPower();
+      markConfigEdited();
+      DBG("TxPowerIdx: %u\n", txPowerIdx);
+    }
+    // Transmit while PTT held
+    if (PTT.press() && !isTx)
+    {
+      rfAudio.transmit();
+      isTx = true;
+      DBG("Transmitting...\n");
+    }
+  }
+  else
+  {
+    if (encoder.turn())
     {
       int step = (encoder.fast() ? 2 : 1) * encoder.dir();
 
+      // Change channel when encoder pressed and turned
       if (encoder.pressing())
       {
-        int c = (int)channel + step;
-        if (c < CH_MIN)
-          c = CH_MAX;
-        if (c > CH_MAX)
-          c = CH_MIN;
-        channel = (uint8_t)c;
+        int new_channel = (int)channel + step;
+        if (new_channel < CH_MIN)
+          new_channel = CH_MAX;
+        if (new_channel > CH_MAX)
+          new_channel = CH_MIN;
+        channel = (uint8_t)new_channel;
         config.channel = channel;
-        radio.setChannel(channel);
-        markCfgDirty();
+        applyChannel();
+        markConfigEdited();
+        DBG("Channel: %u\n", channel);
       }
+      // Change volume when encoder turned
       else
       {
-        int v = (int)volume + step;
-        if (v < 0)
-          v = 0;
-        if (v > 7)
-          v = 7;
-        volume = (uint8_t)v;
+        int new_volume = (int)volume + step;
+        if (new_volume < 0)
+          new_volume = 0;
+        if (new_volume > 7)
+          new_volume = 7;
+        volume = (uint8_t)new_volume;
         config.volume = volume;
-        applyVolume(volume);
-        markCfgDirty();
+        applyVolume();
+        markConfigEdited();
+        DBG("Volume: %u\n", volume);
       }
-
-      break;
     }
-    case EB_CLICK:
-      rateIdx = (rateIdx + 1) % 3;
-      config.rateIdx = rateIdx;
-      applyRadio();
-      markCfgDirty();
-      break;
+
+    // Change rate on encoder click
+    if (encoder.click())
+    {
+      dataRateIdx = (dataRateIdx + 1) % 3;
+      config.dataRateIdx = dataRateIdx;
+      applyDataRate();
+      markConfigEdited();
+      DBG("dataRateIdx: %u\n", dataRateIdx);
     }
   }
 
-  if (PTT.tick())
+  // Stop transmitting when PTT released
+  if (PTT.release() && isTx)
   {
-    switch (PTT.action())
-    {
-    case EB_PRESS:
-    {
-      rfAudio.transmit();
-      break;
-    }
-    case EB_RELEASE:
-    {
-      rfAudio.receive();
-      break;
-    }
-    }
+    rfAudio.receive();
+    isTx = false;
+    DBG("Receiving...\n");
   }
 
-  maybeSaveSettings();
+  saveSettings();
 }
